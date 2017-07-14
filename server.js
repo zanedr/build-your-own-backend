1380a166cf60be24a19033abc48048c2fdf226bc
Zane Russell <zaneroosa@gmail.com>
2017-07-14 11:38:24 -0600

    more slight changes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// const environment = process.env.NODE_ENV || 'production';
 const environment = process.env.NODE_ENV || 'production';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
