99ffa3db5bef20c7cc335df9535aa8e0d9519d2e
Zane Russell <zaneroosa@gmail.com>
2017-07-13 20:24:16 -0600

    maybe

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // const environment = process.env.NODE_ENV || 'development';
-const environment = process.env.NODE_ENV || 'testing';
+const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
