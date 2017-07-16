76a796b3bcb005d0df0bd7c9f14683e49d36dc88
Zane Russell <zaneroosa@gmail.com>
2017-07-15 19:51:50 -0600

    more knex experimentation

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
 const environment = process.env.NODE_ENV || 'production';
-const configuration = require('../knexfile')[environment];
+const config = require('../knexfile')[environment];
 
-module.exports = require('knex')(configuration);
+module.exports = require('knex')(config);
