cb00998ad27face73a9610fca4f6ffb9f35fd3a6
Zane Russell <zaneroosa@gmail.com>
2017-07-15 19:25:05 -0600

    circle ci tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,8 +4,8 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server.js');
 
-// const environment = process.env.NODE_ENV || 'testing';
-const configuration = require(__dirname + '/../knexfile.js').testing;
+const environment = process.env.NODE_ENV || 'testing';
+const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhc3N3b3JkIiwicGFzc3dvcmQiOiJwYXNzd29yZCIsImlhdCI6MTQ5OTk4NjA5OSwiZXhwIjoxNTAxNzE0MDk5fQ.qEAKVrXfIdeEg6Zh5lIkBORfIHmaGRImxccGF1HHDP4';
